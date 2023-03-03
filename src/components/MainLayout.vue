d1ac672e2b36cacb5e20c1cf090930bc1b8f0ef8
Caleb Miller <caleb@mathmammoth.com>
2023-03-03 10:32:34 -0600

    Switch to embedding via events

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,116 +1,133 @@
-<script setup lang="ts">
-import { defineComponent } from "vue";
-import Glayout from "@/components/GoldenLayout.vue";
-import { ItemType, type LayoutConfig } from "golden-layout";
-import Output from "@/components/OutputBox.vue";
+<template>
+  <div ref="element" style="width: 100%; height: 95%;">
+    <teleport v-for="{ id, type, element } in componentInstances" :key="id" :to="element">
+      <component :is="type"></component>
+    </teleport>
+  </div>
+</template>
+<script lang="ts">
+import { useGoldenLayout } from "@/helpers/use-golden-layout";
+import { defineComponent, shallowRef } from "vue";
+import "golden-layout/dist/css/goldenlayout-base.css";
+import "golden-layout/dist/css/themes/goldenlayout-dark-theme.css";
+
+import Output from "./OutputBox.vue";
 import MainCode from "./MainCode.vue";
 import Header from "./HeaderCode.vue";
 import Footer from "./FooterCode.vue";
 import Inputs from "./InputsBox.vue";
 import Flags from "./FlagsBox.vue";
 import CookieClicker from "./CookieClicker.vue";
-</script>
+import { ItemType } from 'golden-layout';
 
-<template>
-  <Glayout ref="gLayoutRoot" style="width: 100%; height: 90%"></Glayout>
-</template>
+const components = { Output, MainCode, Header, Footer, Inputs, Flags, CookieClicker };
 
-<script lang="ts">
-const layout: LayoutConfig = {
-  root: {
-    type: ItemType.row,
-    content: [
-      {
-        type: ItemType.column,
-        size: "100%",
+export default defineComponent({
+  components,
+  setup() {
+    interface ComponentInstance {
+      id: number;
+      type: string;
+      element: HTMLElement;
+    }
+    let instanceId = 0;
+    const componentTypes = new Set(Object.keys(components));
+    const componentInstances = shallowRef<ComponentInstance[]>([]);
+
+    const createComponent = (type: string, element: HTMLElement) => {
+      if (!componentTypes.has(type)) {
+        throw new Error(`Component not found: '${type}'`);
+      }
+      ++instanceId;
+      componentInstances.value = componentInstances.value.concat({
+        id: instanceId,
+        type,
+        element,
+      });
+    };
+    const destroyComponent = (toBeRemoved: HTMLElement) => {
+      console.log(componentInstances.value);
+      componentInstances.value = componentInstances.value.filter(
+        ({ element }) => element !== toBeRemoved
+      );
+    };
+
+    const { element } = useGoldenLayout(createComponent, destroyComponent, {
+      root: {
+        type: ItemType.row,
         content: [
           {
-            type: ItemType.component,
-            title: "Flags",
-            header: { show: "top", popout: false, maximise: false },
-            size: "10%",
-            componentType: Flags,
-          },
-          {
-            type: "stack",
-            size: "50%",
+            type: ItemType.column,
+            size: "100%",
             content: [
               {
                 type: ItemType.component,
-                title: "Code",
+                title: "Flags",
                 header: { show: "top", popout: false, maximise: false },
-                componentType: MainCode,
-                size: "100%",
+                size: "10%",
+                componentType: "Flags",
+              },
+              {
+                type: "stack",
+                size: "50%",
+                content: [
+                  {
+                    type: ItemType.component,
+                    title: "Code",
+                    header: { show: "top", popout: false, maximise: false },
+                    componentType: "MainCode",
+                    size: "100%",
+                  },
+                  {
+                    type: ItemType.component,
+                    title: "Header",
+                    header: { show: "top", popout: false, maximise: false },
+                    componentType: "Header",
+                    size: "100%",
+                  },
+                  {
+                    type: ItemType.component,
+                    title: "Footer",
+                    header: { show: "top", popout: false, maximise: false },
+                    componentType: "Footer",
+                    size: "100%",
+                  },
+                ],
               },
               {
                 type: ItemType.component,
-                title: "Header",
+                title: "Inputs",
                 header: { show: "top", popout: false, maximise: false },
-                componentType: Header,
-                size: "100%",
-              },
-              {
-                type: ItemType.component,
-                title: "Footer",
-                header: { show: "top", popout: false, maximise: false },
-                componentType: Footer,
-                size: "100%",
+                size: "40%",
+                componentType: "Inputs",
               },
             ],
           },
           {
-            type: ItemType.component,
-            title: "Inputs",
-            header: { show: "top", popout: false, maximise: false },
-            size: "40%",
-            componentType: Inputs,
-          },
+            type: ItemType.column,
+            size: "100%",
+            content: [
+              {
+                type: ItemType.component,
+                title: "Output",
+                header: { show: "top", popout: false, maximise: false },
+                size: "60%",
+                componentType: "Output",
+              },
+              {
+                type: ItemType.component,
+                title: "🍪🍪🍪🍪🍪🍪🍪",
+                header: { show: "top", popout: false, maximise: false },
+                size: "40%",
+                componentType: "CookieClicker"
+              }
+            ]
+          }
         ],
       },
-      {
-        type: ItemType.column,
-        size: "100%",
-        content: [
-          {
-            type: ItemType.component,
-            title: "Output",
-            header: { show: "top", popout: false, maximise: false },
-            size: "60%",
-            componentType: Output,
-          },
-          {
-            type: ItemType.component,
-            title: "🍪🍪🍪🍪🍪🍪🍪",
-            header: { show: "top", popout: false, maximise: false },
-            size: "40%",
-            componentType: CookieClicker
-          }
-        ]
-      }
-    ],
-  },
-};
+    });
 
-export default defineComponent({
-  data() {
-    return {
-      code: "",
-      output: "",
-    };
-  },
-  methods: {
-    run() {
-      this.output = "";
-      Vyxal.execute(this.code, "", "", (res) => {
-        this.output += res;
-      });
-    },
-  },
-  mounted() {
-    (this.$refs.gLayoutRoot as any).loadGLLayout(layout);
+    return { element, componentInstances };
   },
 });
 </script>
-
-<style src="golden-layout/dist/css/goldenlayout-base.css"></style>
-<style src="golden-layout/dist/css/themes/goldenlayout-dark-theme.css"></style>

9dbfa87313afff41f7bd9863833f4bc3385b3f57
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-08-23 22:11:52 +1000

    Add the dictionary back

--- src/components/TextCompressor.vue
+++ src/components/TextCompressor.vue
@@ -1,29 +1,17 @@
 <template>
-  <textarea
-    v-model="text"
-    placeholder="Text to compress..."
-    class="p-3 m-4 w-[90%] rounded outline-none text-white focus:ring-4 ring-slate-900 bg-slate-700"
-  />
+  <textarea v-model="text" placeholder="Text to compress..."
+    class="p-3 m-4 w-[90%] rounded outline-none text-white focus:ring-4 ring-slate-900 bg-slate-700" />
   <div class="m-4">
-    <button
-      class="text-white bg-slate-700 px-4 py-3 rounded cursor-pointer hover:bg-slate-800"
-      @click="compress"
-    >
+    <button class="text-white bg-slate-700 px-4 py-3 rounded cursor-pointer hover:bg-slate-800" @click="compress">
       Compress
     </button>
-    <button
-      class="text-white bg-slate-700 px-4 py-3 rounded cursor-pointer hover:bg-slate-800 ml-3"
-      @click="copy"
-      v-if="compressed"
-    >
+    <button class="text-white bg-slate-700 px-4 py-3 rounded cursor-pointer hover:bg-slate-800 ml-3" @click="copy"
+      v-if="compressed">
       Copy to Clipboard
     </button>
   </div>
-  <pre
-    class="m-4 text-white whitespace-pre-wrap font-mono bg-slate-800 p-4 rounded"
-    v-if="compressed"
-    >{{ compressed }}</pre
-  >
+  <pre class="m-4 text-white whitespace-pre-wrap font-mono bg-slate-800 p-4 rounded"
+    v-if="compressed">{{ compressed }}</pre>
 </template>
 
 <script lang="ts">
@@ -38,6 +26,7 @@ export default defineComponent({
   },
   methods: {
     compress() {
+      Vyxal.setLongDict(dictionary.long);
       this.compressed = Vyxal.compress(this.text);
     },
     copy() {

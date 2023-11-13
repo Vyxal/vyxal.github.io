2ea2fd63a8477408f87ca073e9870a73f443d328
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-14 10:37:14 +1100

    Remove everything vue related

--- src/components/MainCode.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import CodeBox from "@/components/CodeBox.vue";
-</script>
-
-<template>
-  <CodeBox data-name="code" :byte-count="true" />
-</template>
--- src/helpers/byte-count.ts
+++
@@ -1,8 +0,0 @@
-export function getByteCount(code: string) {
-  const codepage = Vyxal.getCodepage();
-  const utfable = [...code].every((x) => (codepage + " " + "\n").includes(x));
-  return {
-    utfable,
-    len: utfable ? code.length : new TextEncoder().encode(code).length,
-  };
-}

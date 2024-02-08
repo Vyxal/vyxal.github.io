c37948fa6c14132a69120b0af567b0e885e2dd0b
Ginger <75683114+gingerindustries@users.noreply.github.com>
2024-02-08 15:55:30 +0000

    Remove old code from FlagsDialog

--- src/latest/scripts/dialogs/FlagsDialog.tsx
+++ src/latest/scripts/dialogs/FlagsDialog.tsx
@@ -29,69 +29,14 @@ export const FlagsDialog = memo(function({ flags, setFlags, show, setShow }: Fla
                 [...flagDefs.values()].map((def) => {
                     switch (def.type) {
                         case "boolean": {
-                            return <BooleanSwitch parameter={def.name} flags={flags} setFlags={setFlags} />;
+                            return <BooleanSwitch key={def.name} parameter={def.name} flags={flags} setFlags={setFlags} />;
                         }
                         case "choice": {
-                            return <ChoiceDropdown parameter={def.name} choices={def.choices} flags={flags} setFlags={setFlags} />;
+                            return <ChoiceDropdown key={def.name} parameter={def.name} choices={def.choices} flags={flags} setFlags={setFlags} />;
                         }
                     }
                 })
             }
-            {/* <BooleanSwitch parameter="literate" flags={flags} setFlags={setFlags}>
-                Literate mode
-            </BooleanSwitch>
-            <hr className="mt-1" />
-            <BooleanSwitch parameter="presetStack" flags={flags} setFlags={setFlags}>
-                Preset stack to <code>100</code>
-            </BooleanSwitch>
-            <BooleanSwitch parameter="startRangeAtZero" flags={flags} setFlags={setFlags}>
-                Start ranges at <code>0</code>
-            </BooleanSwitch>
-            <BooleanSwitch parameter="offsetRangeByOne" flags={flags} setFlags={setFlags}>
-                Offset ranges by <code>-1</code>
-            </BooleanSwitch>
-            <BooleanSwitch parameter="rangify" flags={flags} setFlags={setFlags}>
-                Rangify, whatever that means
-            </BooleanSwitch>
-            <BooleanSwitch parameter="fullTrace" flags={flags} setFlags={setFlags}>
-                Full traceback information
-            </BooleanSwitch>
-            <BooleanSwitch parameter="limitPrint" flags={flags} setFlags={setFlags}>
-                Limit printing
-            </BooleanSwitch>
-            <BooleanSwitch parameter="dontEvalInputs" flags={flags} setFlags={setFlags}>
-                Don't evaluate inputs
-            </BooleanSwitch>
-            <InputGroup className="mb-3">
-                <InputGroup.Text>
-                    <label htmlFor="default-arity">Default arity:</label>
-                </InputGroup.Text>
-                <FormSelect
-                    name="default-arity"
-                    className="d-inline w-auto"
-                    value={flags.defaultArity}
-                    onChange={(event) => setFlags({ type: "setting", setting: "defaultArity", value: Number.parseInt(event.currentTarget.value) })}
-                >
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </FormSelect>
-            </InputGroup>
-            <InputGroup className="flex-nowrap">
-                <InputGroup.Text>
-                    <label htmlFor="end-print-mode">End print mode:</label>
-                </InputGroup.Text>
-                <FormSelect
-                    name="end-print-mode"
-                    className="d-inline w-auto"
-                    value={END_PRINT_FLAGS.get(flags.endPrintMode)}
-                    onChange={(event) => setFlags({ type: "setting", setting: "endPrintMode", value: event.currentTarget.value })}
-                >
-                    {[...END_PRINT_MODES.keys()].map((flag) => {
-                        return <option key={flag} value={flag}>{END_PRINT_NAMES.get(flag)}</option>;
-                    })}
-                </FormSelect>
-            </InputGroup> */}
         </Modal.Body>
         <Modal.Footer>
             <Button variant="primary" onClick={() => setShow(false)}>

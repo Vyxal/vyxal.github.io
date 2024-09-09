67c44f0389919673ca78f7b1800932092aeb5d63
gingershaped <75683114+gingershaped@users.noreply.github.com>
2024-09-09 12:42:33 -0400

    Keep the terminal from spawning a new worker every keypress(?!)

--- src/latest/scripts/VyTerminal.tsx
+++ src/latest/scripts/VyTerminal.tsx
@@ -1,4 +1,4 @@
-import React, { ForwardedRef, forwardRef, useContext, useEffect, useImperativeHandle, useRef } from "react";
+import React, { ForwardedRef, forwardRef, useContext, useEffect, useImperativeHandle, useMemo, useRef } from "react";
 import { VyRunner, TerminateReason } from "./runner";
 import { ElementDataContext } from "./util/element-data";
 import splashes from "../data/splash.txt?raw";
@@ -21,31 +21,31 @@ export interface VyTerminalRef {
 const VyTerminal = forwardRef(function VyTerminal({ code, flags, inputs, timeout, onStart, onFinish }: VyTerminalParams, ref: ForwardedRef<VyTerminalRef>) {
     const wrapperRef = useRef(null);
     const elementData = useContext(ElementDataContext);
-    const runner = useRef(new VyRunner(splashes.split("\n"), elementData!.version));
+    const runner = useMemo(() => new VyRunner(splashes.split("\n"), elementData!.version), []);
 
     useImperativeHandle(ref, () => {
         return {
             start() {
-                runner.current.start(code, flags, inputs, timeout);
+                runner.start(code, flags, inputs, timeout);
             },
             stop() {
-                return runner.current.terminate(TerminateReason.Terminated);
+                return runner.terminate(TerminateReason.Terminated);
             },
-            getOutput: runner.current.getOutput,
+            getOutput: runner.getOutput,
         };
     });
 
     useEffect(() => {
-        runner.current.attach(wrapperRef.current!);
-        runner.current.addEventListener("started", onStart);
-        runner.current.addEventListener("finished", onFinish);
+        runner.attach(wrapperRef.current!);
+        runner.addEventListener("started", onStart);
+        runner.addEventListener("finished", onFinish);
         if (code.length > 0) {
-            runner.current.start(code, flags, inputs, timeout);
+            runner.start(code, flags, inputs, timeout);
         }
         return () => {
-            runner.current.detach();
-            runner.current.removeEventListener("started", onStart);
-            runner.current.removeEventListener("finished", onFinish);
+            runner.detach();
+            runner.removeEventListener("started", onStart);
+            runner.removeEventListener("finished", onFinish);
         };
     }, []);
 

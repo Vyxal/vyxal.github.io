3cfde036faf944fbc162ace59eacbc406b96781e
gingershaped <75683114+gingershaped@users.noreply.github.com>
2024-09-10 09:03:47 -0400

    good job lyxal

--- src/latest/scripts/runner.ts
+++ src/latest/scripts/runner.ts
@@ -124,9 +124,6 @@ export class VyRunner extends EventTarget {
             if (this._state == "running") {
                 throw new Error("Attempted to start while running");
             }
-            if (code == "lyxal") {
-                window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-            }
             this.terminal?.clear();
             this.outputBuffer.length = 0;
             worker.postMessage({

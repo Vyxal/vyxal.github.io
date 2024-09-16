caec0777cbb02949017e00f378184905c1bf7873
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-09-16 10:54:01 -0400

    thanks git

---
+++ src/latest/scripts/extensions/vyxal/bracket-matching.ts
@@ -0,0 +1,150 @@
+import { syntaxTree } from "@codemirror/language";
+import { EditorState, Range, StateField } from "@codemirror/state";
+import { Decoration, DecorationSet, WidgetType } from "@codemirror/view";
+import { SyntaxNode } from "@lezer/common";
+import { EditorView } from "codemirror";
+
+const theme = EditorView.baseTheme({
+    "&.cm-focused .cm-matchingBracket": { backgroundColor: "#328c8252" },
+    "&.cm-focused .cm-nonmatchingBracket": { backgroundColor: "#bb555544" },
+    "&:not(.cm-focused) .cm-eofMarker": { display: "none" },
+    ".cm-eofMarker": { display: "inline" },
+});
+
+const matchingMark = Decoration.mark({class: "cm-matchingBracket"});
+const nonmatchingMark = Decoration.mark({class: "cm-nonmatchingBracket"});
+
+class EOFWidget extends WidgetType {
+    eq() {
+        return true;
+    }
+    toDOM(): HTMLElement {
+        const element = document.createElement("span");
+        element.classList.add("cm-eofMarker");
+        element.classList.add("cm-placeholder");
+        element.classList.add("cm-matchingBracket");
+        element.textContent = "␄";
+        return element;
+    }
+}
+
+const eofWidget = Decoration.widget({
+    widget: new EOFWidget(),
+    side: 1,
+});
+
+const enum Side {
+    FORWARDS = 1, BACKWARDS = -1,
+}
+
+type MatchResult = { 
+    matched: true,
+    start: SyntaxNode[],
+    end: SyntaxNode | null,
+} | {
+    matched: false,
+    highlight: SyntaxNode,
+} | null;
+
+function matchBrackets(state: EditorState, position: number, side: Side): MatchResult {
+    const tree = syntaxTree(state);
+    const startNode = tree.resolveInner(position, side);
+    if (startNode.type.name == "StructureOpen") {
+        const iter = tree.cursorAt(position, side);
+        let depth = 0;
+        do {
+            console.log(iter.name);
+            if (iter.name == "StructureClose") {
+                const closer = state.doc.sliceString(iter.from, iter.to);
+                if (closer == "}") {
+                    depth--;
+                } else if (closer == ")") {
+                    depth -= 2;
+                } else if (closer == "]") {
+                    depth = 0;
+                }
+                if (depth <= 0) {
+                    return { matched: true, start: [startNode], end: iter.node };
+                }
+            } else if (iter.name == "StructureOpen") {
+                depth++;
+            }
+        } while (iter.next(true));
+        return { matched: true, start: [startNode], end: null };
+    } else if (startNode.type.name == "StructureClose") {
+        let openers: SyntaxNode[] = [];
+        tree.iterate({
+            enter(node) {
+                if (node.name == "StructureClose") {
+                    const closer = state.doc.sliceString(node.from, node.to);
+                    if (closer == "}") {
+                        openers.splice(openers.length - 2);
+                    } else if (closer == ")") {
+                        openers.pop();
+                    } else if (closer == "]") {
+                        openers = [];
+                    }
+                } else if (node.name == "StructureOpen") {
+                    openers.push(node.node);
+                }
+            },
+            from: 0,
+            to: startNode.from - 1,
+        });
+        const closer = state.doc.sliceString(startNode.from, startNode.to);
+        if (closer == "}") {
+            if (openers.length >= 1) {
+                return { matched: true, start: [openers.pop()!], end: startNode };
+            }
+        } else if (closer == ")") {
+            if (openers.length >= 2) {
+                return { matched: true, start: openers.splice(openers.length - 2), end: startNode };
+            }
+        } else if (closer == "]") {
+            if (openers.length > 0) {
+                return { matched: true, start: openers, end: startNode };
+            }
+        } else {
+            throw new Error(`Unexpected StructureClose ${closer}`);
+        }
+        return { matched: false, highlight: startNode };
+    }
+    return null;
+}
+
+const bracketMatchingState = StateField.define<DecorationSet>({
+    create() {
+        return Decoration.none;
+    },
+    update(decorationSet, transaction) {
+        if (!transaction.docChanged && !transaction.selection) {
+            return decorationSet;
+        }
+        const decorations: Range<Decoration>[] = [];
+        for (const range of transaction.state.selection.ranges) {
+            if (!range.empty) {
+                continue;
+            }
+            const match = matchBrackets(transaction.state, range.head, Side.FORWARDS) ?? matchBrackets(transaction.state, range.head, Side.BACKWARDS);
+            console.log(match);
+            if (match) {
+                if (match.matched) {
+                    match.start.map((node) => decorations.push(matchingMark.range(node.from, node.to)));
+                    if (match.end) {
+                        decorations.push(matchingMark.range(match.end.from, match.end.to));
+                    } else {
+                        decorations.push(eofWidget.range(transaction.state.doc.length));
+                    }
+                } else {
+                    decorations.push(nonmatchingMark.range(match.highlight.from, match.highlight.to));
+                }
+            } 
+        }
+        return Decoration.set(decorations, true);
+    },
+    provide: (f) => EditorView.decorations.from(f),
+});
+
+export function vyxalBracketMatching() {
+    return [theme, bracketMatchingState];
+}
---
+++ src/latest/scripts/extensions/vyxal/vyxal-extensions.ts
@@ -0,0 +1,89 @@
+import type { CompletionContext, CompletionResult } from "@codemirror/autocomplete";
+import { LanguageSupport, syntaxTree } from "@codemirror/language";
+import type { ElementData } from "../../util/element-data";
+import type { UtilWorker } from "../../util/util-worker";
+import { elementAutocomplete, elementTooltip } from '../common';
+import type { SyntaxNode } from "@lezer/common";
+import { Decoration, DecorationSet, EditorView, hoverTooltip } from "@codemirror/view";
+import { EditorState, Extension, MapMode, Range, StateField } from "@codemirror/state";
+import { vyxalLanguage } from "../../../../common/scripts/languages/vyxal";
+import { vyxalBracketMatching } from "./bracket-matching";
+
+export function vyxalCompletion(elementData: ElementData) {
+    return vyxalLanguage.data.of({
+        autocomplete(context: CompletionContext): Promise<CompletionResult | null> {
+            const sugar = context.matchBefore(/#[,.^](.)/);
+            if (sugar != null) {
+                const desugared = elementData.sugars.get(sugar.text);
+                if (typeof desugared == "string") {
+                    return Promise.resolve({
+                        from: sugar.from,
+                        filter: false,
+                        options: [
+                            { label: desugared, detail: "sugar trigraph", type: "constant" },
+                        ],
+                    });
+                }
+            }
+            return elementAutocomplete(elementData, context, false);
+        },
+    });
+}
+
+export function vyxalHover(util: UtilWorker): Extension {
+    async function makeStringTooltip(view: EditorView, node: SyntaxNode): Promise<HTMLElement | null> {
+        if (node.name != "String") {
+            return Promise.resolve(null);
+        }
+        const content = view.state.doc.slice(node.from, node.to).toString();
+        switch (content.at(-1)) {
+            case "\"": {
+                return Promise.resolve(null);
+            }
+            case "„": {
+                const decompressed = await util.decompress(content);
+                const container = document.createElement("div");
+                container.innerHTML = `<b>Compressed string: </b> ${decompressed}`;
+                return container;
+            }
+            default:
+                return Promise.resolve(null);
+        }
+    }
+    const stringTooltip = hoverTooltip(async(view, pos) => {
+        const node = syntaxTree(view.state).resolve(pos);
+        const element = await makeStringTooltip(view, node);
+        if (element != null) {
+            const container = document.createElement("div");
+            container.appendChild(element);
+            return {
+                pos: pos,
+                create: () => {
+                    return {
+                        dom: container,
+                        update: async(update) => {
+                            if (update.changes.touchesRange(node.from, node.to)) {
+                                const newPos = update.changes.mapPos(pos, -1, MapMode.TrackDel);
+                                if (newPos != null) {
+                                    const element = await makeStringTooltip(
+                                        view,
+                                        syntaxTree(view.state).resolve(newPos),
+                                    );
+                                    if (element != null) {
+                                        container.replaceChildren(element);
+                                    }
+                                }
+                            }
+                        },
+                    };
+                },
+            };
+        }
+        return null;
+    });
+    return [stringTooltip];
+}
+
+export function vyxal(util: UtilWorker, data: ElementData) {
+    return new LanguageSupport(vyxalLanguage, [vyxalCompletion(data), vyxalHover(util), elementTooltip(data, false), vyxalBracketMatching()]);
+}

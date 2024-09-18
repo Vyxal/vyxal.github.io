37a87312de771e786adb439d7762c40ca0bfc2ee
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-09-18 17:03:23 -0400

    Fix small border issue with thing cards that have no modifiers

--- src/latest/scripts/ThingCard.tsx
+++ src/latest/scripts/ThingCard.tsx
@@ -19,21 +19,21 @@ export function ThingCard({ thing, shadow, onClick }: ThingCardProps) {
         <Card.Body>
             <Card.Title>{thing.symbol}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{thing.name}</Card.Subtitle>
-            {thing.type != "syntax" ? (
+            {(thing.type != "syntax" && thing.keywords.length > 0) && (
                 <Card.Text>
                     {thing.keywords.map((keyword, i) => <code key={i} className="code-pill">{keyword}</code>)}
                 </Card.Text>
-            ) : null}
-            {thing.type != "element" ? (
-                <Card.Text>{thing.description}</Card.Text>
-            ) : null}
+            )}
+            {thing.type != "element" && <Card.Text>{thing.description}</Card.Text>}
         </Card.Body>
-        <ListGroup variant="flush">
-            {thing.type != "syntax" ? (
-                thing.overloads.map((overload, i) => {
-                    return <ListGroup.Item className="font-monospace" key={i}>{overload}</ListGroup.Item>;
-                })
-            ) : <ListGroup.Item className="font-monospace">{thing.usage}</ListGroup.Item>}
-        </ListGroup>
+        {(thing.type == "syntax" || thing.overloads.length > 0) && 
+            <ListGroup variant="flush">
+                {thing.type != "syntax" ? (
+                    thing.overloads.map((overload, i) => {
+                        return <ListGroup.Item className="font-monospace" key={i}>{overload}</ListGroup.Item>;
+                    })
+                ) : <ListGroup.Item className="font-monospace">{thing.usage}</ListGroup.Item>}
+            </ListGroup>
+        }
     </Card>;
 }
